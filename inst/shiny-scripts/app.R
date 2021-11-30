b8c4999c3269a86c5f7f3b79d067efaa5767dd04
Kuai Yu <quick2063706271@outlook.com>
2021-11-30 02:00:49 -0500

    Update app.R

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -1,8 +1,40 @@
 library(shiny)
 ui <- fluidPage(
-  # ... code
+  # Upload files (fileInput)
+  selectInput(inputId = "select",
+              label = "Select regions of your interest",
+              choices = c("Wuhan" = "Wuhan",
+                          "Canada" = "Canada",
+                          "Italy" = "Italy",
+                          "USA" = "USA",
+                          "Kenya" = "Kenya",
+                          "Bahrain" = "Bahrain",
+                          "Germany" = "Germany",
+                          "Pakistan" = "Pakistan",
+                          "Britain" = "Britain",
+                          "Thailand" = "Thailand"
+                          ),
+              selected = c("Wuhan", "Canada"),
+              multiple = TRUE
+  ),
+
+  # Select country/genome (selectInput)
+  fileInput(inputId = "fastaFile", label = "Upload your Fasta file",
+            accept = c(
+              ".fasta"
+            )
+  ),
+  fileInput(inputId = "nameToCountryFile", label = "Upload nameToCountry file",
+            accept = c(
+              ".txt",
+              ".csv"
+            )
+  ),
+  # Output heatmap + plot
 )
 server <- function(input, output) {
-  # ... code
+  output$result <- renderText({
+    paste("You chose", input$select)
+  })
 }
 shiny::shinyApp(ui = ui, server = server)
